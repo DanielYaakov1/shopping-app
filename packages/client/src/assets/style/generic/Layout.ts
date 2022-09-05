ba59b0229c44294ad96ba31559da42013dd44e06
DanielYaakov1 <danielyaakov91@gmail.com>
2022-09-05 22:24:32 +0300

    feat: add new modal for cart shopping + rename props

--- packages/client/src/assets/style/generic/Layout.ts
+++ packages/client/src/assets/style/generic/Layout.ts
@@ -1,7 +1,15 @@
 import style from 'styled-components';
 
 export const LayoutStyle = style.main`
-               display:inline-block;
-               width: 100%;
-               height: 100%;      
-           `;
+position: fixed;
+top: 15vh;
+left: 5%;
+width: 90%;
+background-color: white;
+padding: 1rem;
+border-radius: 14px;
+box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
+z-index: 30;
+animation: slide-down 700ms ease-out forwards;    
+
+`;
