889bc80a65c9b63fc0b4e2b0bc798965c652cba7
DanielYaakov1 <danielyaakov91@gmail.com>
2022-09-07 23:28:27 +0300

    feat: add net error handling middleware  + fix logic for fetch  all product + sorting product

--- packages/server/src/api/controllers/auth-controller.ts
+++ packages/server/src/api/controllers/auth-controller.ts
@@ -1,16 +1,17 @@
-import type { Request, Response } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import { auth } from '../../firebase';
 import { FirebaseHandler } from '../handlers/FirebaseHandler';
 
-export const checkUser = async (req: Request, res: Response) => {
+export const checkUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).send(res.locals.user);
   } catch (err) {
-    res.send(err);
+    next(err);
+    //res.send(err);
   }
 };
 
-export const loginFirebase = async (req: Request, res: Response) => {
+export const loginFirebase = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email, password } = req.body;
     const authHandler = new FirebaseHandler();
@@ -20,11 +21,12 @@ export const loginFirebase = async (req: Request, res: Response) => {
     res.header('authorization-bearer', userCredential.token);
     res.send(userCredential);
   } catch (err) {
-    res.send(err);
+    next(err);
+    //res.send(err);
   }
 };
 
-export const signupFirebase = async (req: Request, res: Response) => {
+export const signupFirebase = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email, password } = req.body;
     const authHandler = new FirebaseHandler();
@@ -34,17 +36,19 @@ export const signupFirebase = async (req: Request, res: Response) => {
     res.header('authorization-bearer', userCredential.token);
     res.send(userCredential);
   } catch (err) {
-    res.send(err);
+    next(err);
+    //res.send(err);
   }
 };
 
-export const checkAuth = async (req: Request, res: Response) => {
+export const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.body;
     const authHandler = new FirebaseHandler();
     const response = await authHandler.checkAuth(id);
     res.send(response);
   } catch (err) {
-    res.send(err);
+    next(err);
+    //res.send(err);
   }
 };
