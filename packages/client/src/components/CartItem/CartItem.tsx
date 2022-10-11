993217b255577852f2049e1354360095f271081f
DanielYaakov1 <danielyaakov91@gmail.com>
2022-10-11 23:57:40 +0300

    feat : add admins schema + reset all cart states + add styling for cart buttons

--- packages/client/src/components/CartItem/CartItem.tsx
+++ packages/client/src/components/CartItem/CartItem.tsx
@@ -10,6 +10,24 @@ const useStyles = makeStyles(() =>
       borderBottom: '2px solid #8a2b06',
       padding: '1rem 0',
       margin: '1rem 0',
+      '& button': {
+        font: 'inherit',
+        fontWeight: 'bold',
+        fontSize: '1rem',
+        color: '#8a2b06',
+        border: '1px solid #8a2b06',
+        width: '2rem',
+        textAlign: 'center',
+        borderRadius: 6,
+        backgroundColor: 'transparent',
+        cursor: 'pointer',
+        marginLeft: '1rem',
+        margin: '0.25rem',
+      },
+      '& button:active': {
+        color: 'white',
+        backgroundColor: '#8a2b06',
+      },
     },
 
     summary: {
