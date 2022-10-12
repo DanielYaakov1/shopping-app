ae59cbf7094d653e1367f64e8c001dddbbaa7d71
DanielYaakov1 <danielyaakov91@gmail.com>
2022-10-13 01:01:55 +0300

    feat: adding admin logic backend + front, fix state cart

--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -4,6 +4,7 @@ import { appReducer } from './slices/appSlice';
 import { productReducer } from './slices/ProductSlice';
 import { cartReducer } from './slices/cartSlice';
 import { orderReducer } from './slices/orderSlice';
+import { userReducer } from './slices/userSlice';
 
 export const store = configureStore({
   reducer: {
@@ -12,6 +13,7 @@ export const store = configureStore({
     productReducer: productReducer,
     cartReducer: cartReducer,
     orderReducer: orderReducer,
+    userReducer: userReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
