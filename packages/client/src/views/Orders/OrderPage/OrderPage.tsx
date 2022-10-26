1f87de323ca40f825ede6885960546527ae586b0
DanielYaakov1 <danielyaakov91@gmail.com>
2022-10-26 23:38:51 +0300

    feat: adding order card component + styling

--- packages/client/src/views/Orders/OrderPage/OrderPage.tsx
+++ packages/client/src/views/Orders/OrderPage/OrderPage.tsx
@@ -1,31 +1,19 @@
-import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ActionsOrder from '../../../actions/OrdersAction';
 import Spinner from '../../../components/Spinner/Spinner';
-import { IShippingOrder } from '../../../interfaces';
 import { setOrders } from '../../../store/slices/orderSlice';
 import { RootState } from '../../../store/store';
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    cartItem: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      borderBottom: '2px solid #8a2b06',
-      padding: '1rem 0',
-      margin: '1rem 0',
-    },
-  })
-);
+import { CardOrder } from '../../../components/CardOrder/CardOrder';
+import { OrdersStyle } from '../../../assets/style/components/OrdersStyle';
 
 export type Props = {
   className?: string;
   children?: React.ReactNode;
 };
+
 const Order = ({ className, children }: Props) => {
-  const classes = useStyles();
+  const classes = OrdersStyle();
   const dispatch = useDispatch();
   const { getOrderById } = ActionsOrder();
   const uid = useSelector((state: RootState) => state.userReducer.uid);
@@ -43,36 +31,8 @@ const Order = ({ className, children }: Props) => {
 
   return (
     <div>
-      <div>
-        <h1>Orders</h1>
-        {!isLoadingOrder ? (
-          orders.map((order: IShippingOrder, i: number) => (
-            <div key={i} className={classes.cartItem}>
-              #{i + 1}
-              <div>
-                <div>city: {order.city}</div>
-                <div>street: {order.street}</div>
-                <div>zipCode: {order.zipCode}</div>
-              </div>
-              <div>description: {order.description}</div>
-              <div>createdAt: {order.createdAt}</div>
-              <div>Arrival date: {order.shippingDate}</div>
-              <div>
-                Items:
-                {order.items.map((item: any) => (
-                  <div>
-                    <div>name:{item.name}</div>
-                    <div>{item.amount}</div>
-                    <div>price:{item.price}</div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))
-        ) : (
-          <Spinner />
-        )}
-      </div>
+      <h1>Orders</h1>
+      {!isLoadingOrder ? <CardOrder classes={classes} orders={orders} /> : <Spinner />}
     </div>
   );
 };
