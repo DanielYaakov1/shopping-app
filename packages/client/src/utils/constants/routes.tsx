dd80cc6f3d38066cc35c8fe736665e2585808d35
DanielYaako1 <daniely@moveo.co.il>
2023-01-24 22:35:04 +0200

    feat: implement logout endpoint from + backend /  add dialog alerts

--- packages/client/src/utils/constants/routes.tsx
+++ packages/client/src/utils/constants/routes.tsx
@@ -1,15 +1,18 @@
 export enum ROUTES {
-  LOGIN_API = '/api/v1/auth/login',
+  //routes react-router
   LOGIN_PAGE = '/login',
-  SIGNUP = '/api/v1/auth/signup',
   ORDERS = '/orders',
   ABOUT = '/about',
+  HOME_PAGE = '/',
+  PAGE_NOT_FOUND = '*',
+  //api endpoint server
+  LOGIN_API = '/api/v1/auth/login',
+  SIGNUP = '/api/v1/auth/signup',
   CHECK_AUTH_USER = '/api/v1/auth/check-auth',
   CHECK_TOKEN_EXPIRED_API = '/api/v1/auth/check-token-expired',
   PRODUCTS_API = '/api/v1/products',
   ORDERS_API = '/api/v1/orders/',
-  HOME_PAGE = '/',
-  PAGE_NOT_FOUND = '*',
   AUTHORIZATION_API = '/api/v1/auth',
   PAGINATION_API = '/api/v1/products/get-products-count',
+  LOGOUT_API = '/api/v1/auth/logout',
 }
