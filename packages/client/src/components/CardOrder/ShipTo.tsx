da34a90cc2cf47bbe33002b69cbfa62c4e6c2357
DanielYaako1 <daniely@moveo.co.il>
2023-03-07 23:01:33 +0200

    add: logic for address  comp

--- packages/client/src/components/CardOrder/ShipTo.tsx
+++ packages/client/src/components/CardOrder/ShipTo.tsx
@@ -1,33 +1,16 @@
 import React, { useState } from 'react';
+import useStyles from './useStyles';
 
 function ShipTo() {
-  const [isHovering, setIsHovering] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovering(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovering(false);
-  };
+  const classes = useStyles();
 
   return (
-    <div
-      style={{
-        backgroundColor: 'wheat',
-        borderRadius: '5px',
-        padding: '2px 2px ',
-        width: '250px',
-        maxHeight: 'none',
-        opacity: 1,
-        // top: '20px',
-        // left: '20px',
-        // position: 'absolute',
-        display: 'block',
-      }}>
-      <p>John Doe</p>
-      <p>123 Main St</p>
-      <p>Anytown, USA 12345</p>
+    <div className={classes.shipToContainer}>
+      <div className={classes.shipToContext}>
+        <p>John Doe</p>
+        <p>123 Main St</p>
+        <p>Anytown, USA 12345</p>
+      </div>
     </div>
   );
 }
